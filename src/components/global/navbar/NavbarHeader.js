fb12a9478bbe2d9d45ee1d5e1b29f4d3bab0a842
Brienyll <Brienyll29@gmail.com>
2020-01-19 23:04:06 -0800

    added navbar header component

--- src/components/global/navbar/NavbarHeader.js
+++ src/components/global/navbar/NavbarHeader.js
@@ -1,9 +1,40 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
-import logo from "../../.."
+import logo from "../../../images/logo.webp"
+import { GiHamburgerMenu } from "react-icons/gi"
+import styled from "styled-components"
 
 export default class NavbarHeader extends Component {
   render() {
-    return <div>navbar header</div>
+    const { handleNavbar } = this.props
+    return (
+      <HeaderWrapper>
+        <Link to="/">
+          <img src={logo} alt="Burger Resto" />
+        </Link>
+        <GiHamburgerMenu
+          className="toggle-icon"
+          onClick={() => {
+            handleNavbar()
+          }}
+        />
+      </HeaderWrapper>
+    )
   }
 }
+
+const HeaderWrapper = styled.div`
+  padding: 0.4rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .toggle-icon {
+    font-size: 1.75rem;
+    cursor: pointer;
+  }
+  @media (min-width: 768px) {
+    .toggle-icon {
+      display: none;
+    }
+  }
+`
